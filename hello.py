af4ec29b4929c4a06228c41b8c1e30aa1aea0769
lha01 <lifehackapps.01@gmail.com>
2019-02-19 14:13:20 +0900

    end

--- hello.py
+++ hello.py
@@ -8,8 +8,10 @@ from flask_sockets import Sockets
 from gevent.event import AsyncResult
 from time import sleep
 import requests
+import json
+import random
 #event処理の設定
-massage = AsyncResult()
+message = AsyncResult()
 # __name__は現在のファイルのモジュール名
 app = Flask(__name__)
 sockets = Sockets(app)
@@ -21,6 +23,8 @@ def index():
 	return render_template('index1.html')
 @app.route('/shenron',methods=['POST'])
 def post():
+	i = random.randint(1,3)
+	i = str(i)
 	req = request.json['queryResult']['parameters']['Target']
 	#characterの分岐
 	if req == 'character':
@@ -28,30 +32,70 @@ def post():
 		color = request.json['queryResult']['parameters']['Color']
 		cascol = req+','+custom+','+color
 		# massage.get()
-		massage.set(cascol)
+		message.set(cascol)
 		gevent.sleep(0)
-		result = jsonify({"fulfillmentText": '<speak><audio src = "https://gsya-80fd3.firebaseapp.com/1.wav"/><sub alias="">test</sub></speak>'})
+		result = jsonify({"fulfillmentText": '<speak><audio src = "https://gsya-80fd3.firebaseapp.com/'+i+'.wav"/><sub alias="">test</sub></speak>'})
 		return make_response(result)
 	elif req == 'game':
 		ans = request.json['queryResult']['parameters']['num']
-		if ans is None:
+		if ans == "":
 			data=requests.get('https://us-central1-dungeon-3317e.cloudfunctions.net/quizResponchi?text=game')
-			return make_response(data.text)
+			result = jsonify({"fulfillmentText": '<speak><audio src = "https://gsya-80fd3.firebaseapp.com/'+i+'.wav"/><sub alias="">'+data.text+'</sub></speak>'})
+			return make_response(result)
 		else :
 			data=requests.get('https://us-central1-dungeon-3317e.cloudfunctions.net/quizResponchi?text='+ans)
-			return make_response(data.text)
+			dataArray=data.text.split(',')
+			#画面に表示するデータ
+			text=dataArray[0]
+			#判定処理に使うデータ
+			judge=dataArray[1]
+			gamereq= req+','+judge
+			message.set(gamereq)
+			gevent.sleep(0)
+			if judge == "true":
+				result = jsonify({"fulfillmentText": '<speak><audio src = "https://gsya-80fd3.firebaseapp.com/game_true.wav"/><sub alias="">'+text+'</sub></speak>'})
+				return make_response(result)
+			else:
+				result = jsonify({"fulfillmentText": '<speak><audio src = "https://gsya-80fd3.firebaseapp.com/game_false.wav"/><sub alias="">'+text+'</sub></speak>'})
+			return make_response(result)
 	elif req == 'lifehack':
 		material = request.json['queryResult']['parameters']['material']
-		if material is None:
-			data = requests.get('https://script.google.com/macros/s/AKfycbyQfg85J9SR2KwH6yHOA4xrflDIy8LB5a313Fal-my7WgxjZw/exec')
-			return make_response(data.text)
-		else :
-			data = requests.get('https://script.google.com/macros/s/AKfycbyQfg85J9SR2KwH6yHOA4xrflDIy8LB5a313Fal-my7WgxjZw/exec?text='+material)
-			return make_response(data.text)
-	else:
-		massage.set(req)
+		data = requests.get('https://script.google.com/macros/s/AKfycbyQfg85J9SR2KwH6yHOA4xrflDIy8LB5a313Fal-my7WgxjZw/exec?text='+material)
+		#一度dict型に変換し、stringに変換
+		data=data.json()
+		title = data["title"]
+		url = data["url"]
+		img = data["img"]
+		result = jsonify({"fulfillmentText": '<speak><audio src = "https://gsya-80fd3.firebaseapp.com/'+i+'.wav"/><sub alias="">'+'料理名は'+title+'urlは'+url+'だよ！'+'</sub></speak>'},
+		{
+			"platform": "ACTIONS_ON_GOOGLE",
+			"basicCard": {
+				"image": {
+					"imageUri": img,
+					"accessibilityText": title
+				}
+			}
+		})
+		return make_response(result)
+	elif req == 'greeting':
+		message.set(req)
 		gevent.sleep(0)
-		result = jsonify({"fulfillmentText": req})
+		sleep(2)
+		result = jsonify({"fulfillmentText": '<speak><audio src = "https://gsya-80fd3.firebaseapp.com/greeting.wav"/><sub alias="">test</sub></speak>'})
+		return make_response(result)
+	elif req == 'jump':
+		message.set(req)
+		gevent.sleep(0)
+		result = jsonify({"fulfillmentText": '<speak><audio src = "https://gsya-80fd3.firebaseapp.com/'+i+'.wav"/><sub alias="">test</sub></speak>'})
+		return make_response(result)
+	elif req == 'action':
+		message.set(req)
+		gevent.sleep(0)
+		result = jsonify({"fulfillmentText": '<speak><audio src = "https://gsya-80fd3.firebaseapp.com/'+i+'.wav"/><sub alias="">test</sub></speak>'})
+		return make_response(result)
+	else:
+		gevent.sleep(0)
+		result = jsonify({"fulfillmentText": '<speak><audio src = "https://gsya-80fd3.firebaseapp.com/'+i+'.wav"/><sub alias="">ok</sub></speak>'})
 		return make_response(result)
 #websocket側
 @sockets.route('/shenron')
@@ -60,11 +104,11 @@ def ws(ws):
 		ws = request.environ['wsgi.websocket']
 		while True:
 			gevent.sleep(0)
-			ms=massage.get()
+			ms=message.get()
 			if ms is not None:
 				print("ms:"+ms)
 				ws.send(ms)
-				massage.set()
+				message.set()
 			else:
 				ws.receive()
 		return ''
